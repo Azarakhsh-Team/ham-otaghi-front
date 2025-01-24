55c578df472847bf0017af6ffe001060ae8f6e1d
yasamangh <yasy.gh1999@gmail.com>
2025-01-24 11:57:20 +0330

    add integration

---
+++ src/app/survey/survey-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SurveyPageComponent, SurveyQuestionsPageComponent } from './pages';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'start',
+  },
+  {
+    path: 'start',
+    component: SurveyPageComponent,
+  },
+  {
+    path: 'questions',
+    component: SurveyQuestionsPageComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class SurveyRoutingModule {}
